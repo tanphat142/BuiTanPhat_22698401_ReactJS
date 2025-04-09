555d3386fe53f2527d24fdf1bc91a3887de39bba
Tan Phat <btphat11321@gmail.com>
2025-04-09 16:43:32 +0700

    5. isActive-NavLink; 6. React Router; 7. SPA -Single Page Application

--- BuiTanPhat_22698401_Tuan06/lab06/src/App.jsx
+++ BuiTanPhat_22698401_Tuan06/lab06/src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 
 import SideBar from "./components/Sidebar";
 import DashBoard from "./pages/Dashboard";
+import Teams from "./pages/Teams";
+import Projects from "./pages/Projects";
+import Analytics from "./pages/Analytics";
+import Messages from "./pages/Messages";
+import Integrations from "./pages/Integrations";
 
 const App = () => {
   return (
@@ -11,7 +17,14 @@ const App = () => {
       </div>
       <div>
         <Header />
-        <DashBoard />
+        <Routes>
+          <Route path="/" element={<DashBoard />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/integrations" element={<Integrations />} />
+        </Routes>
       </div>
     </div>
   )
--- BuiTanPhat_22698401_Tuan06/lab06/src/components/SideBar.jsx
+++ BuiTanPhat_22698401_Tuan06/lab06/src/components/SideBar.jsx
@@ -6,30 +6,30 @@ const SideBar = () => {
         <div className="p-5 border-r-1 border-r-gray-300">
             <img src={images.Image_1858} alt="" />
             <div className="mt-5 flex flex-col gap-5 text-gray-500">
-                <a  className="flex gap-3 px-3 py-1.5 rounded">
+                <NavLink to='/' className="flex gap-3 px-3 py-1.5 rounded">
                     <img src={images.Squares_four_1} alt="Squares_four_1" className="text-gray-700" />
                     <p>Dashboard</p>
-                </a>
-                <a  className="flex gap-3 px-3 py-1.5 rounded">
+                </NavLink>
+                <NavLink to='/projects' className="flex gap-3 px-3 py-1.5 rounded">
                     <img src={images.Folder} alt="Folder" />
                     <p>Projects</p>
-                </a>
-                <a  className="flex gap-3 px-3 py-1.5 rounded">
+                </NavLink>
+                <NavLink to='/teams' className="flex gap-3 px-3 py-1.5 rounded">
                     <img src={images.Groups} alt="Groups" />
                     <p>Teams</p>
-                </a>
-                <a className="flex gap-3 px-3 py-1.5 rounded">
+                </NavLink>
+                <NavLink to='/analytics' className="flex gap-3 px-3 py-1.5 rounded">
                     <img src={images.Pie_chart} alt="Pie_chart" />
                     <p>Analytics</p>
-                </a>
-                <a  className="flex gap-3 px-3 py-1.5 rounded">
+                </NavLink>
+                <NavLink to='/messages' className="flex gap-3 px-3 py-1.5 rounded">
                     <img src={images.Chat} alt="Chat" />
                     <p>Messages</p>
-                </a>
-                <a  className="flex gap-3 px-3 py-1.5 rounded">
+                </NavLink>
+                <NavLink to='/integrations' className="flex gap-3 px-3 py-1.5 rounded">
                     <img src={images.Code} alt="Code" />
                     <p>Integrations</p>
-                </a>
+                </NavLink>
             </div>
             <div className="mt-20 bg-[#EFF6FF] p-5 flex flex-col gap-5 items-center">
                 <img src={images.Group} alt="Group" />
